4a3c2991d55d060df9114f259122a74477d7c3b3
Jenna Goldman <jenna.goldman89@gmail.com>
2020-06-17 01:51:49 -0700

    add ImageDetail and pass title and imageSource props to it

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = () => {
-  return (
-    <Text>ImageDetail</Text>
-  );
+const ImageDetail = (props) => {
+  return <View>
+    <Image source={props.imageSource} />
+    <Text>{props.title}</Text>
+  </View>
+  ;
 };
 
 const styles = StyleSheet.create({});
