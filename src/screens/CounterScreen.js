fdb2d5e105f1f2d6df5d4d09c56f5540cd682686
Jenna Goldman <jenna.goldman89@gmail.com>
2020-06-17 12:21:54 -0700

    clean up reducer in CounterScreen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -3,28 +3,29 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 
 const reducer = (state, action) => {
   switch(action.type) {
-    case 'increase_count':
-      return state + action.payload;
-    case 'decrease_count':
-      return state - action.payload;
+    case 'increment':
+      return { ...state, count: state.count + action.payload };
+    case 'decrement':
+      return { ...state, count: state.count + action.payload };
     default:
       return state;
   }
 }
 
 const CounterScreen = () => {
-  const [state, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const { count } = state;
 
   return <View>
     <Button 
       title="Increase" 
-      onPress={() => dispatch({ type: 'increase_count', payload: 1 })}
+      onPress={() => dispatch({ type: 'increment', payload: 1 })}
     />
     <Button 
       title="Decrease"
-      onPress={() => dispatch({ type: 'increase_count', payload: 1 })}
+      onPress={() => dispatch({ type: 'decrement', payload: -1 })}
     />
-    <Text>Current Count: {state}</Text>
+    <Text>Current Count: {count}</Text>
   </View>;
 };
 
